019c13c500c01dd41f4946812b18e7034a1bd1a6
Diego Alexandre <ddsa.1988@outlook.com>
2023-02-14 23:45:22 -0300

    commit

--- 003_recursion.js
+++ 003_recursion.js
@@ -1,13 +1,31 @@
 "use strict";
 
-function printNumber(number) {
-    if (typeof number !== "number") {
-        throw new TypeError("Function argument mus be a number!");
+{
+    function printNumber(number) {
+        if (typeof number !== "number") {
+            throw new TypeError("Function argument mus be a number!");
+        }
+
+        console.log(number);
+
+        return number > 0 ? printNumber(--number) : 0;
     }
 
-    console.log(number);
-
-    return number > 0 ? printNumber(--number) : 0;
+    printNumber(5);
 }
 
-printNumber(5);
+console.log();
+
+{
+    const factorial = (number) => {
+        if (typeof number !== "number") {
+            throw new TypeError("Function argument mus be a number!");
+        }
+        if (number <= 1) return 1;
+
+        return number * factorial(number - 1);
+    };
+
+    console.log(factorial(4));
+    console.log(factorial(7));
+}
